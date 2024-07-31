8ff85c4d1d8461cde86de886aea6a6ebb4a2c4ce
Porter Young <porteryoung2005@gmail.com>
2024-07-31 12:17:57 -0600

    restaurantSchema  added to db context

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { RestaurantSchema } from '../models/Restaurant.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Restaurant = mongoose.model('Restaurant', RestaurantSchema)
 }
 
 export const dbContext = new DbContext()
