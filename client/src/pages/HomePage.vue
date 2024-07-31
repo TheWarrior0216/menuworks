f3c61bc883b075b3b1f381769fe7825ef7e83787
Zach Barnes <zachary.barnes.32@gmail.com>
2024-07-31 12:35:52 -0600

    Created landing section on home page with basic styling

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -4,9 +4,56 @@
 </script>
 
 <template>
-  <h1>This is the home page</h1>
+  <!-- Landing Section -->
+  <section class="container">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center align-items-center landing-card">
+        <div class="text-center">
+          <h1 class="landing-title">MenuWorks</h1>
+          <h2 class="landing-subtitle">Mobile Ordering Made Easy</h2>
+          <button class="btn btn-dark landing-button">Register</button>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Featured Restaurant Section -->
+  <section class="container">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center">
+        <div class="featuredRestaurant">
+          <h2 class="featured-restaurant">Featured Restaurant</h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
+
+.landing-card {
+  padding-top: 5em;
+  padding-bottom: 5em;
+}
+
+.landing-title {
+  font-size: 56px;
+  font-weight: 600;
+}
+
+.landing-subtitle {
+  color: gray;
+  font-size: 28px;
+}
+
+.landing-button {
+  padding-left: 6em;
+  padding-right: 6em;
+  margin-top: 1em;
+}
+
+.featured-restaurant {
+  font-size: 36px;
+}
 
 </style>
