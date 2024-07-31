2f44be87f12804e1f1423ffae45dc32ca89ff33e
Seth Baxter <TheWarrior0216@gmail.com>
2024-07-31 12:07:51 -0600

    Item Done

---
+++ server/src/models/Item.js
@@ -0,0 +1,11 @@
+import { Schema } from "mongoose";
+
+export const ItemSchema = new Schema({
+  restaurantId: { type: Schema.ObjectId, required: true, ref: 'Restraunt' },
+  name: { type: String, required: true, maxLength: 20, },
+  picture: { type: String, required: true, maxLength: 200, },
+  price: { type: Number, min: 0, max: 1000, required: true },
+  description: { type: String, required: true, maxLength: 200, },
+  isAvailable: { type: Boolean, required: true, default: true },
+  type: { type: String, enum: [`breakfast`, `lunch`, `dinner`, `burgers`, `pizza`, `sushi`, `sandwiches`, `salads`, `seafood`, `barbecue`, `breakfast`, `brunch`, `desserts`, `ice cream`, `wings`, `vegetarian`, `vegan`, `healthy`, `fast food`, `comfort food`, `noodles`, `pasta`, `poke`, `ramen`, `smoothies`, `coffee`, `bakery`, `donuts`, `fried chicken`, `tacos`, `burritos`, `soup`, `steak`, `seafood`, `wraps`, `kebabs`, `halal`], required: true }
+})
