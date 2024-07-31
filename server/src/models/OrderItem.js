dad2651d627899dc01f3735d81f49b6fa98df317
Porter Young <porteryoung2005@gmail.com>
2024-07-31 15:38:34 -0600

    orderItem Schema

---
+++ server/src/models/OrderItem.js
@@ -0,0 +1,15 @@
+import { Schema } from "mongoose";
+
+export const OrderItemsSchema = new Schema({
+    orderId: { type: Schema.ObjectId, required: true },
+    itemId: { type: Schema.ObjectId, required: true },
+    quantity: { type: Number, required: true, default: 1, min: 1, max: 99 },
+    specialInstructions: { type: String, maxLength: 250 }
+}, { toJSON: { virtuals: true } })
+
+OrderItemsSchema.virtual('item', {
+    foreignField: '_id',
+    ref: 'order',
+    localField: 'orderId',
+    justOne: true
+})
