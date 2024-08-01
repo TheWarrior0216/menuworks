d92075a55974eb4bfa3d8b6f36cd10bd32aee6fe
Seth Baxter <TheWarrior0216@gmail.com>
2024-08-01 13:08:07 -0600

    Get Order by Id

--- server/src/controllers/OrdersController.js
+++ server/src/controllers/OrdersController.js
@@ -6,8 +6,27 @@ export class OrdersController extends BaseController {
   constructor() {
     super('api/orders')
     this.router
+      .get('', this.getAllOrders)
+      .get('/:orderId', this.getSpecificOrder)
       .use('', Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createOrder)
+  }
+  async getAllOrders(req, res, next) {
+    try {
+      const orders = await ordersService.getAllOrders()
+      res.send(orders)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async getSpecificOrder(req, res, next) {
+    try {
+      const orderId = req.params.orderId
+      const order = await ordersService.getSpecificOrder(orderId)
+      res.send(order)
+    } catch (error) {
+      next(error)
+    }
   }
   async createOrder(req, res, next) {
     try {
--- server/src/services/OrdersService.js
+++ server/src/services/OrdersService.js
@@ -1,6 +1,14 @@
 import { dbContext } from "../db/DbContext.js"
 
 class OrdersService {
+  async getAllOrders() {
+    const orders = await dbContext.Orders.find()
+    return orders
+  }
+  async getSpecificOrder(orderId) {
+    const order = await dbContext.Orders.findById(orderId)
+    return order
+  }
   async createOrder(orderData) {
     const order = await dbContext.Orders.create(orderData)
     return order
