2c2dec0570e4b6b6001ca2185b61de10a85d37d0
Porter Young <porteryoung2005@gmail.com>
2024-07-31 23:39:52 -0600

    added comment for dummy data in restaurant schema

--- server/src/models/Restaurant.js
+++ server/src/models/Restaurant.js
@@ -23,11 +23,46 @@ export const RestaurantSchema = new Schema({
 
 // {
 //     "name": "Mcdonalds",
-//     "description": "this is a test of the post request for restaurants",
-//     "primaryPictureURL": "https://images.unsplash.com/photo-1606720335177-3d04e70fb13b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//     "location": "21520 Ustick rd caldwell idaho",
-//     "hours": [{"open": 2, "close": 3, "day": "monday"},{"open": 2, "close": 3, "day": "tuesday"},{"open": 2, "close": 3, "day": "wednesday"},{"open": 2, "close": 3, "day": "thursday"},{"open": 2, "close": 3, "day": "friday"},{"open": 2, "close": 3, "day": "saturday"},{"open": 2, "close": 3, "day": "sunday"}],
-//     "type": "american",
-//     "primaryColor": "#000000",
-//     "logoURL": "https://images.unsplash.com/photo-1619454016518-697bc231e7cb?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+//         "description": "this is a test of the post request for restaurants",
+//             "primaryPictureURL": "https://images.unsplash.com/photo-1606720335177-3d04e70fb13b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//                 "location": "21520 Ustick rd caldwell idaho",
+//                 {
+//                     "hours": [
+//                         {
+//                             "open": "10:00",
+//                             "close": "21:00",
+//                             "day": "Monday"
+//                         },
+//                         {
+//                             "open": "10:00",
+//                             "close": "21:00",
+//                             "day": "Tuesday"
+//                         },
+//                         {
+//                             "open": "10:00",
+//                             "close": "21:00",
+//                             "day": "Wednesday"
+//                         },
+//                         {
+//                             "open": "10:00",
+//                             "close": "21:00",
+//                             "day": "Thursday"
+//                         },
+//                         {
+//                             "open": "10:00",
+//                             "close": "21:00",
+//                             "day": "Friday"
+//                         },
+//                         {
+//                             "open": "9:00",
+//                             "close": "23:00",
+//                             "day": "Saturday"
+//                         }, {
+//                             "open": "9:00", "close": "23:00", "day": "Sunday"
+//                         }
+//                     ]
+//                 },
+//                     "type": "american",
+//                         "primaryColor": "#000000",
+//                             "logoURL": "https://images.unsplash.com/photo-1619454016518-697bc231e7cb?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 // }
