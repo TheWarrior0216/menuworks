3af9f41bfd751f1c15e6c902fa7b5e1e57508140
Porter Young <porteryoung2005@gmail.com>
2024-08-05 11:58:33 -0600

    restaurants card greyscale on closed

--- client/src/components/RestaurantCard.vue
+++ client/src/components/RestaurantCard.vue
@@ -12,7 +12,19 @@ defineProps({ restaurant: Restaurant })
 
 <template>
     <RouterLink :to="{ name: 'Restaurant Details', params: { restaurantId: restaurant.id } }">
-        <div class="restaurant-card mb-4 shadow rounded border">
+        <div v-if="restaurant?.isOpen" class="restaurant-card mb-4 shadow rounded border">
+            <img :src="restaurant.primaryPictureURL" class="card-img-top rounded-top" :alt="restaurant.name">
+            <div class="card-body p-3">
+                <h5 class="card-title">{{ restaurant.name }}</h5>
+                <p class="card-text mb-2">{{ restaurant.location }}</p>
+                <div v-for="hours in restaurant.hours" :key="hours.day">
+                    <p v-if="hours.day == AppState.currentDay" class="m-0 p-0">{{ hours.open }} - {{ hours.close }}</p>
+                </div>
+                <p class="text-info fw-bold">{{ restaurant.type }}</p>
+            </div>
+        </div>
+
+        <div v-else class="restaurant-card mb-4 shadow rounded border" style="filter: grayscale()">
             <img :src="restaurant.primaryPictureURL" class="card-img-top rounded-top" :alt="restaurant.name">
             <div class="card-body p-3">
                 <h5 class="card-title">{{ restaurant.name }}</h5>
