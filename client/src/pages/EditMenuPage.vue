661a144abb27de7bee48eb17d9b7161d34267e13
Zach Barnes <zachary.barnes.32@gmail.com>
2024-08-05 10:13:36 -0600

    Created the edit menu page

---
+++ client/src/pages/EditMenuPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+
+<template>
+<h1>This is the edit menu page where restaurant owners can go ads, edit, delete menu items</h1>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
