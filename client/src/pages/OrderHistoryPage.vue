29e842e45f0776fb2f35b222ebb0f2a20400db9d
Porter Young <porteryoung2005@gmail.com>
2024-08-06 13:43:38 -0600

    getting orders on order history page

--- client/src/pages/OrderHistoryPage.vue
+++ client/src/pages/OrderHistoryPage.vue
@@ -1,15 +1,37 @@
 <script setup>
+import { computed, onMounted } from 'vue';
+import { ordersService } from '../services/OrdersService.js';
+import { useRoute } from 'vue-router';
+import Pop from '../utils/Pop.js';
+import { AppState } from '../AppState.js';
+
+const route = useRoute()
+
+const orders = computed(()=>AppState.orders)
+
+onMounted(() => {
+    getOrders()
+})
+
+async function getOrders() {
+    try {
+        await ordersService.getRestaurantOrders(route.params.restaurantId)
+    }
+    catch (error) {
+        Pop.error(error);
+    }
+
+}
 
 </script>
 
 
 <template>
 
-<h1>This page will show all of the order history for the restaurant's online orders</h1>
+    <h1>This page will show all of the order history for the restaurant's online orders</h1>
+    <h2>{{ orders }}</h2>
 
 </template>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
