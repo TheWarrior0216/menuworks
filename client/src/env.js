9583029835b27eba2aa6585fd15c4676b475afe7
Porter Young <porteryoung2005@gmail.com>
2024-08-09 11:46:56 -0600

    deploy: env.js changed to my info

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-fyu7pxzwsr65gvex.us.auth0.com'
-export const clientId = 'mimbhClKKzPQJu5oe4pywHfD7PT8dHzv'
-export const audience = 'https://mirage/'
+export const domain = 'dev-hhcu2170fadrfsij.us.auth0.com'
+export const clientId = '9ky5h2ESpCSBmkWByEuMajyVhAiNBsZN'
+export const audience = 'https://PortersAuthTest/'
