abe91698a2cc108b487f285fff08f99a68242145
Porter Young <porteryoung2005@gmail.com>
2024-08-09 12:00:55 -0600

    jeremy is a sick little dude

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000' : 'https://boisemenuworks.com'
 export const useSockets = false
 export const domain = 'dev-hhcu2170fadrfsij.us.auth0.com'
 export const clientId = '9ky5h2ESpCSBmkWByEuMajyVhAiNBsZN'
